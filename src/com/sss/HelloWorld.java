94bf9ea07ffa1c35d6d6743484c5a1676830d6fe
slk-xcj <114073090+slk-xcj@users.noreply.github.com>
2022-09-22 18:36:05 +0800

    Update HelloWorld.java
    网页端修改

--- src/com/sss/HelloWorld.java
+++ src/com/sss/HelloWorld.java
@@ -2,6 +2,6 @@ package com.sss;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorldAAA");
     }
 }
