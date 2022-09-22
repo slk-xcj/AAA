13987c80d62d2fd4e6396c88d0bb50c1db29487c
slk-xcj <cuiyuliang_edu@163.com>
2022-09-22 18:42:23 +0800

    BBB

--- src/com/sss/HelloWorld.java
+++ src/com/sss/HelloWorld.java
@@ -2,6 +2,6 @@ package com.sss;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorldAAA");
+        System.out.println("HelloWorldAAABBB");
     }
 }
