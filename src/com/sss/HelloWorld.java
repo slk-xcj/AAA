85622c7d34a1bdb9c82356159d2daf001fe4719b
slk-xcj <cuiyuliang_edu@163.com>
2022-09-23 14:04:43 +0800

    DDD

--- src/com/sss/HelloWorld.java
+++ src/com/sss/HelloWorld.java
@@ -2,6 +2,6 @@ package com.sss;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorldAAABBB");
+        System.out.println("HelloWorldAAABBBDDD");
     }
 }
